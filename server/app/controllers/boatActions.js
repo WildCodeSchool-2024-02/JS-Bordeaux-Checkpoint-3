16a1a8e21728b1fdf550b6c48740f2d2b624b573
Enzo Michel <enzomichel399@gmail.com>
2024-06-10 11:58:40 +0200

    finished step 3

--- server/app/controllers/boatActions.js
+++ server/app/controllers/boatActions.js
@@ -13,6 +13,18 @@ const browse = async (req, res, next) => {
   }
 };
 
+const edit = async (req, res, next) => {
+  const boat = { id: req.params.id, coord_x : req.body.coord_x, coord_y : req.body.coord_y};
+  try {
+    await tables.boat.update(boat);
+
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   browse,
+  edit,
 };
--- server/app/routers/api/boats/router.js
+++ server/app/routers/api/boats/router.js
@@ -6,10 +6,12 @@ const router = express.Router();
 // Define Your API Routes Here
 /* ************************************************************************* */
 
-const { browse } = require("../../../controllers/boatActions");
+const { browse, edit } = require("../../../controllers/boatActions");
+const validateTuile = require("../../../services/tileExists");
 
-router.get("/", browse);
+router.get("/", browse, validateTuile);
 
+router.put("/:id", edit, validateTuile);
 /* ************************************************************************* */
 
 module.exports = router;
