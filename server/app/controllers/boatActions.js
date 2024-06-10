b09e440ea2987bc4941361e759b4408526fbdcf6
JulienChevreauPro <julienchevreau33@gmail.com>
2024-06-10 11:21:43 +0200

    step 3

--- server/app/controllers/boatActions.js
+++ server/app/controllers/boatActions.js
@@ -13,6 +13,22 @@ const browse = async (req, res, next) => {
   }
 };
 
+const edit = async (req, res, next) => {
+  // Extract the item data from the request body
+  const boat = req.body;
+  try {
+    // Insert the item into the database
+    const insertId = await tables.boat.update(boat, req.params.id);
+
+    // Respond with HTTP 201 (Created) and the ID of the newly inserted item
+    res.status(204).json({ insertId });
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
 module.exports = {
   browse,
+  edit,
 };
