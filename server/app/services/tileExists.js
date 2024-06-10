a735b16e1373a003bbf787d697995fc00bc9299c
sylvain.lafaye <sylvain-lafaye@hotmail.fr>
2024-06-10 12:32:38 +0200

    finished step4

--- server/app/services/tileExists.js
+++ server/app/services/tileExists.js
@@ -1,1 +1,11 @@
-module.exports = null;
+
+const tileExist = (req, res, next) => {
+    const coordx = req.body.coord_x
+    const coordy = req.body.coord_y
+    if (coordx >= 0 && coordx <= 11 && coordy >= 0 && coordy <= 5){
+        next()
+    } else {
+        res.sendStatus(422);
+    }
+}
+module.exports = tileExist;
