2cc1e57cc21963e6d28afd2361f546133a5b5505
Rollet Fabien <fabrol47@gmail.com>
2024-06-10 09:52:28 +0200

    test 2 done

--- server/app/controllers/tileActions.js
+++ server/app/controllers/tileActions.js
@@ -1,1 +1,14 @@
-module.exports = {};
+const tables = require("../../database/tables");
+
+const browse = async (req, res, next) => {
+  try {
+    const tile = await tables.tile.readAll();
+    res.json(tile);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  browse,
+};
